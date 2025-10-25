26285c9fe05b746713b873037a34ca620af0d476
Mr.DCT <verlaberinyuy8@gmail.com>
2025-10-25 12:32:06 +0100

    reactive declarations

--- src/App.svelte
+++ src/App.svelte
@@ -1,125 +1,51 @@
 <!--	JavaScript	-->
+<!--
+In Svelte, a reactive declaration is a special syntax that allows you to declare variables whose values automatically recompute whenever their dependencies change. This is a core part of Svelte's reactivity system, enabling efficient updates to the UI without the need for a virtual DOM.
+The syntax for a reactive declaration uses a labeled statement with a dollar sign and a colon ($:), followed by the variable declaration and its assignment.-->
 <script>
-	const formValues = {
-		name: "",
-		profileSummary: "",
-		country: '',
-		jobLocation: [],
-		remoteWork: false,
-		skillSet: [],
-		yearsOfExperience: '',
-	}
+  import { identity } from "svelte/internal";
 
-	function submitForm() {
-		console.log(formValues)
-	}
+	let firsName = "Verla"
+	let lastName = "Berinyuy"
+	$: fullName = `${firsName} ${lastName}`
+
+	let items = [
+		{
+			id: 1,
+			title: 'TV',
+			price: 100
+		},
+		{
+			id: 2,
+			title: 'Phone',
+			price: 200
+		},
+		{
+			id: 3,
+			title: 'Laptop',
+			price: 300
+		}
+	]
+	$: total = items.reduce((total, curr) => (total = total + curr.price), 0)
 </script>
 
 <!--	HTML	-->
 <main>
-	<div>
-		<pre>
-			{JSON.stringify(formValues, null, 2)}
-		</pre>
-	</div>
-	<form on:submit|preventDefault={submitForm}><!--We specify a modifier using a pipe character-->
-		<div>
-			<label for="name">Name</label>
-			<input type="text" id="name" bind:value={formValues.name}/>
-		</div>
-
-		<div>
-			<label for="profile">Profile</label>
-			<textarea name="" id="profile" bind:value={formValues.profileSummary}/>
-		</div>
-
-		<div>
-			<label for="country">Country</label>
-			<select name="" id="country" bind:value={formValues.country}>
-				<option value="">Select a country</option>
-				<option value="cameroon">Cameroon</option>
-				<option value="rwanda">Rwanda</option>
-				<option value="nigeria">Nigeria</option>
-			</select>
-		</div>
-
-		<div>
-			<label for="job-location">Job Location</label>
-			<select name="" id="job-location" bind:value={formValues.jobLocation} multiple>
-				<option value="">Select a country</option>
-				<option value="cameroon">Cameroon</option>
-				<option value="rwanda">Rwanda</option>
-				<option value="nigeria">Nigeria</option>
-				<option value="gana">Gana</option>
-			</select>
-		</div>
-
-		<div>
-			<input type="checkbox" name="" id="remoteWork" bind:checked={formValues.remoteWork} />
-			<label for="remoteWork">Open for remote work?</label>
-		</div>
-
-		<div>
-			<label for="">Skill set</label>
-			<input 
-			type="checkbox" 
-			id="html" 
-			value="html" 
-			bind:group={formValues.skillSet} 
-			/>
-			<label for="html">HTML</label>
-			<input 
-			type="checkbox" 
-			id="css" 
-			value="css" 
-			bind:group={formValues.skillSet} 
-			/>
-			<label for="css">CSS</label>
-			<input 
-			type="checkbox" 
-			id="javascript" 
-			value="javascript" 
-			bind:group={formValues.skillSet} 
-			/>
-			<label for="javascript">JavaScript</label>
-		</div>
-
-		<div>
-			<label for="">Years of Experience</label>
-			<input 
-			type="radio" 
-			id="0-2" 
-			value="0-2" 
-			bind:group={formValues.yearsOfExperience} 
-			/>
-			<label for="0-2">0-2</label>
-			<input 
-			type="radio" 
-			id="3-5" 
-			value="3-5" 
-			bind:group={formValues.yearsOfExperience} 
-			/>
-			<label for="3-5">3-5</label>
-			<input 
-			type="radio" 
-			id="6-10" 
-			value="6-10" 
-			bind:group={formValues.yearsOfExperience} 
-			/>
-			<label for="6-10">6-10</label>
-		</div>
-
-		<div>
-			<button>Submit</button>
-		</div>
-	</form>
+	<button on:click={() => {
+		firsName = 'Rejoice'
+		lastName = 'Ngu'
+	}}>Change name</button>
+	<h2>{firsName} {lastName}</h2>
+	<h1>{fullName}</h1>
+	<button on:click={() => (items = [...items, {id: 4, title: 'keyboard', price: 50}])}
+	>Add item</button>
+	<h2>
+		Total - {total}
+	</h2>
 </main>
 
 <!--	CSS		-->
 <style>
-	input + label {
-		display: inline-flex;
-	}
 	main {
 		text-align: center;
 		padding: 1em;
