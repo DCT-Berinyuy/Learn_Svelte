d4ef94b7dcd4d35fe1a2d38c4ab89163756ee00c
Mr.DCT <verlaberinyuy8@gmail.com>
2025-10-27 05:22:52 +0100

    context API

--- svelte-components/src/App.svelte
+++ svelte-components/src/App.svelte
@@ -2,25 +2,21 @@
 <!--Context API provides a way, to pass data through the component tree without having to pass props down manually at every level-->
 
 <script>
-	import Greet from "./components/Greet.svelte";
-	const name = "Berinyuy"
-	const channel = "DCT Lab"
-	const obj = {
-		name: 'Berry',
-		heroName: 'Flash'
-	}
+	import { setContext } from "svelte";
+	import ComponentC from "./components/ComponentC.svelte";
+	const userName = "Mr.DCT"
+	setContext('username-context', userName)
+	//Note: The two arguments to setContext right now both are string values
+	//but they could be any dataType that your app requres it to be.
+	//The 1st argument in fact, what is recommended is to not use a string as the key
+	//but instead use an Object literal. Garantees no conflicts
 </script>
 
 <!--	HTML	-->
 
 <main>
-	<Greet name="Verla" heroName="Iron man" />
-	<Greet name="Ngu" heroName="Spider man"/>
-	<Greet name ="DCT" />
-	<Greet {name} heroName={channel} />
-	<Greet name={obj.name} heroName={obj.heroName} />
-	<!--By using the spread operator-->
-	<Greet {...obj} />
+	<h2>App Component username - {userName}</h2>
+	<ComponentC />
 </main>
 
 <!--	CSS		-->
@@ -31,13 +27,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
