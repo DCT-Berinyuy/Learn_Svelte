e89149fbdae1ef6f3c932f31a83af864f68aaec2
Mr.DCT <verlaberinyuy8@gmail.com>
2025-10-27 10:37:59 +0100

    component/custom event handler

--- svelte-components/src/App.svelte
+++ svelte-components/src/App.svelte
@@ -2,21 +2,22 @@
 <!--Context API provides a way, to pass data through the component tree without having to pass props down manually at every level-->
 
 <script>
-	import { setContext } from "svelte";
-	import ComponentC from "./components/ComponentC.svelte";
-	const userName = "Mr.DCT"
-	setContext('username-context', userName)
-	//Note: The two arguments to setContext right now both are string values
-	//but they could be any dataType that your app requres it to be.
-	//The 1st argument in fact, what is recommended is to not use a string as the key
-	//but instead use an Object literal. Garantees no conflicts
+	import Popup from "./components/Popup.svelte";
+	let showPopup = false;
+
+	function closPopup(event) {
+		showPopup = false;
+		console.log(event.detail)
+	}
 </script>
 
 <!--	HTML	-->
 
 <main>
-	<h2>App Component username - {userName}</h2>
-	<ComponentC />
+	<button on:click={() => (showPopup = true)}>Show Popup</button>
+	{#if showPopup}
+		<Popup on:close={closPopup} />
+	{/if}
 </main>
 
 <!--	CSS		-->
---
+++ svelte-components/src/components/Popup.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+</script>
+
+<div>
+    <h2>This is a popup</h2>
+    <button on:click={() => dispatch('close', "Mr.DCT")}>Close Popup</button>
+</div>
