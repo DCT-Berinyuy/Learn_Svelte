c127ae54f0f64825c1f244068cf4a96c165327d9
Mr.DCT <verlaberinyuy8@gmail.com>
2025-11-04 13:59:40 +0100

    Lifecycle hooks

--- svelte-components/src/App.svelte
+++ svelte-components/src/App.svelte
@@ -1,5 +1,16 @@
 <!--	JavaScript	-->
 
+<!--
+A component's lifecycle refers to the phasese a component
+goes through in a Svelte application.
+Lifecycle hooks are functions that allows us run code at key moments
+during that lifecycle. There are a total of 5 lifecycle
+hooks that we can use in a component
+1. The onMount hook: The unMount function schedules the passed
+in call back to run as soon as the component been mounted to th DOM
+2. The onDestroy hook: This schedule a call back 
+-->
+
 <script>
 import ChildStyle from "./components/ChildStyle.svelte";
 </script>
@@ -7,7 +18,7 @@ import ChildStyle from "./components/ChildStyle.svelte";
 <!--	HTML	-->
 
 <main>
-	<h3>ChildStyle component global style</h3>
+	<h3>App component global style</h3>
 	<h4>Appp component text</h4>
 	<ChildStyle />
 
